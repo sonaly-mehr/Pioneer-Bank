9786749ade4d023f9629dc1f4b82922619f59213
sonaly-mehr <sonaly.mehr@gmail.com>
2021-07-02 01:03:15 +0600

    second commit

---
+++ main.js
@@ -0,0 +1,4 @@
+document.getElementById("login").addEventListener("click", function(){
+    //    document.getElementById("login-section").style.display= "none";
+    console.log("clicked");
+       })
